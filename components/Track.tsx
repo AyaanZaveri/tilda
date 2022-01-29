b174ce7c79b646c9e58131e69df2461bb6350470
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-29 16:36:46 -0500

    Changing Colors

--- components/Track.tsx
+++ components/Track.tsx
@@ -9,12 +9,12 @@ const Track = ({ key, track }: Props) => {
   return (
     <div
       key={key}
-      className="mt-2 flex w-80 flex-col rounded-lg border bg-white px-3 py-2 text-sm shadow-lg transition hover:bg-slate-50 hover:text-emerald-700"
+      className="mt-2 flex w-80 h-48 flex-col rounded-lg border bg-white px-3 pt-2 text-sm transition hover:bg-slate-50 hover:text-slate-700"
     >
-      <span className="inline-flex font-medium text-emerald-900">
+      <span className="inline-flex font-medium text-slate-900">
         {track.track_name}
       </span>
-      <span className="font-normal text-emerald-500">{track.artist_name}</span>
+      <span className="font-normal text-slate-500">{track.artist_name}</span>
     </div>
   )
 }
