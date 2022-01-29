ee37d63e1f5bcf2ef1532937d0b20c9d77f7b840
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-29 16:50:55 -0500

    Semibold font

--- components/Track.tsx
+++ components/Track.tsx
@@ -11,7 +11,7 @@ const Track = ({ key, track }: Props) => {
       key={key}
       className="mt-2 flex w-80 h-48 flex-col rounded-lg border bg-white px-3 pt-2 text-sm shadow-sm transition hover:bg-slate-50 hover:text-slate-700"
     >
-      <span className="inline-flex font-medium text-slate-900">
+      <span className="inline-flex font-semibold text-slate-900">
         {track.track_name}
       </span>
       <span className="font-normal text-slate-500">{track.artist_name}</span>
