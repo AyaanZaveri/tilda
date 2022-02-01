6e5852b9a05c792fefbd95260d81372f9182128b
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-01 16:15:49 -0500

    Fixing Comments

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -4,13 +4,15 @@ import { NextResponse } from 'next/server'
 export async function middleware(req: any) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET! })
 
-  // If the token exists, and valid.
+  // If the token is valid.
   if (req.nextUrl.pathname.includes('/api/auth') || token) {
     // Continue to the app.
     return NextResponse.next()
   }
 
+  // If the token is invalid.
   if (!token && req.nextUrl.pathname !== '/login') {
+    // Redirect to the login page.
     return NextResponse.redirect('/login')
   }
 }
