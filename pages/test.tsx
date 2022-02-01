50e3eb26506b2fae3703679e6280f9938c42efd9
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-31 22:00:51 -0500

    Deleting test.tsx

--- pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { useSession, signIn, signOut } from 'next-auth/react'
-
-const Test = () => {
-  const { data: session } = useSession()
-
-  return <div>{session}</div>
-}
-
-export default Test
