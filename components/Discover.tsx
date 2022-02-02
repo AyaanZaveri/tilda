65736c8c502989eaedfbbaa945826c77a404769e
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-02 17:23:08 -0500

    Mapping Playlists

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -2,22 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { IconCompass } from '@tabler/icons'
 
 const Discover = ({ spotifyApi }: any) => {
-  const [playlist, setPlaylist] = useState<any>([])
-  const getPlaylist = () => {
-    spotifyApi.getPlaylist('37i9dQZEVXbMDoHDwVN2tF').then(
-      function (data) {
-        console.log('Some information about this playlist', data.body)
-        setPlaylist(data.body)
-      },
-      function (err) {
-        console.log('Something went wrong!', err)
-      }
-    )
-  }
+  const charts = ['37i9dQZEVXbMDoHDwVN2tF', '37i9dQZEVXbKj23U1GF4IR']
 
-  useEffect(() => {
-    getPlaylist()
-  }, [])
+  const [playlist, setPlaylist] = useState<any>([])
 
   return (
     <div className="ml-3 flex flex-col gap-5">
@@ -26,11 +13,23 @@ const Discover = ({ spotifyApi }: any) => {
         <IconCompass className="mt-1 h-8 w-8" />
       </div>
       <div className="flex flex-row flex-wrap">
-        <div className="flex h-48 w-48 flex-col gap-3 rounded-lg bg-gradient-to-b from-blue-900 to-sky-800">
-          <span className="mb-3 inline-flex h-full w-full items-end justify-center font-bold text-white">
-            {playlist.name}
-          </span>
-        </div>
+        {charts.map((chart: any) => {
+          const getPlaylist = () => {
+            spotifyApi.getPlaylist(chart).then(
+              function (data) {
+                console.log('Some information about this playlist', data.body)
+                setPlaylist(data.body)
+              },
+              function (err) {
+                console.log('Something went wrong!', err)
+              }
+            )
+          }
+
+          useEffect(() => {
+            getPlaylist()
+          }, [])
+        })}
       </div>
     </div>
   )
