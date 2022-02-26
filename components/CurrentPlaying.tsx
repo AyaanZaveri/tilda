6308ab3884deac42cba02ba2269d7248ac9207c3
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-26 15:04:59 -0500

    Album artwork and Artist names

--- components/CurrentPlaying.tsx
+++ components/CurrentPlaying.tsx
@@ -3,13 +3,21 @@ import { useState } from 'react'
 
 interface CurrentlyPlaying {
   name: string
+  album: {
+    images: {
+      url: string
+    }
+  }
+  artists: {
+    name: string
+  }[]
 }
 
 const CurrentPlaying = ({ spotifyApi }) => {
   const [currentTrack, setCurrentTrack] = useState<CurrentlyPlaying>()
 
   const getCurrentTrack = () => {
-    spotifyApi.getMyCurrentPlayingTrack().then(
+    spotifyApi!.getMyCurrentPlayingTrack().then(
       function (data) {
         data.body ? setCurrentTrack(data.body.item) : ''
       },
@@ -22,14 +30,24 @@ const CurrentPlaying = ({ spotifyApi }) => {
   useEffect(() => {
     setInterval(() => {
       getCurrentTrack()
-    }, 1000)
+    }, 2000)
   }, [])
 
   console.log(currentTrack)
 
   return (
     <div className="fixed bottom-0 w-full border-t border-gray-800 bg-slate-900 p-5">
-      <span className="text-white">{currentTrack?.name}</span>
+      <div className="flex flex-row items-center gap-3">
+        <img
+          className="w-14 rounded-lg"
+          src={currentTrack?.album.images[0].url}
+          alt=""
+        />
+        <div className='flex flex-col'>
+          <span className="text-white">{currentTrack?.name}</span>
+          <span className="text-white text-sm font-light">{currentTrack?.artists.map(artist => artist.name).join(", ")}</span>
+        </div>
+      </div>
     </div>
   )
 }
