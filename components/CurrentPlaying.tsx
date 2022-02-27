b94d6b4a64d9f99d8fc0834d6c625f1ce10eaf1b
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-26 19:05:18 -0500

    Hover Effect

--- components/CurrentPlaying.tsx
+++ components/CurrentPlaying.tsx
@@ -4,12 +4,14 @@ import { useState } from 'react'
 interface CurrentlyPlaying {
   name: string
   album: {
+    id: string
     images: {
       url: string
     }
   }
   artists: {
     name: string
+    id: string
   }[]
 }
 
@@ -38,13 +40,15 @@ const CurrentPlaying = ({ spotifyApi }) => {
   return (
     <div>
       {currentTrack ? (
-        <div className="fixed bottom-0 w-full border-t border-gray-800 bg-slate-900 bg-opacity-75 backdrop-blur p-5">
+        <div className="fixed bottom-0 w-full border-t border-gray-800 bg-slate-900 bg-opacity-75 p-5 backdrop-blur">
           <div className="flex flex-row items-center gap-3">
-            <img
-              className="w-14 rounded-md"
-              src={currentTrack?.album.images[0].url}
-              alt=""
-            />
+            <a href={`/album/${currentTrack.album.id}`}>
+              <img
+                className="w-14 rounded-md shadow-2xl transition-all hover:brightness-110"
+                src={currentTrack?.album.images[0].url}
+                alt=""
+              />
+            </a>
             <div className="flex flex-col">
               <span className="font-medium text-white">
                 {currentTrack?.name}
@@ -59,8 +63,9 @@ const CurrentPlaying = ({ spotifyApi }) => {
                   ) : (
                     ''
                   ),
+
                   <span className="text-sm font-light text-white transition-all hover:cursor-pointer hover:underline">
-                    {artist.name}
+                    <a href={`/artist/${artist.id}`}>{artist.name}</a>
                   </span>,
                 ])}
               </div>
