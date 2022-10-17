c32b338dfe98662ecba62a95bfe36b9c788c1079
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-10-17 19:25:23 -0400

    Favourite Album Tracks

--- components/AlbumTrack.tsx
+++ components/AlbumTrack.tsx
@@ -11,6 +11,10 @@ import { currentTrackState } from "../atoms/songAtom";
 import { pipedApiUrl, tildaApiUrl } from "../utils/apiUrl";
 import { playingTrackState } from "../atoms/playingTrack";
 import { PauseIcon, PlayIcon } from "@heroicons/react/24/solid";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../firebase";
+import { addDoc, collection, deleteDoc, doc } from "firebase/firestore";
+import { useCollection } from "react-firebase-hooks/firestore";
 
 interface Props {
   track: any;
@@ -20,6 +24,7 @@ interface Props {
 
 const AlbumTrack = ({ track, index, thumbnails }: Props) => {
   const router = useRouter();
+  const [user] = useAuthState(auth);
 
   const [currentTrack, setCurrentTrack] = useRecoilState(currentTrackState);
   const [playingTrack, setPlayingTrack] = useRecoilState(playingTrackState);
@@ -49,6 +54,45 @@ const AlbumTrack = ({ track, index, thumbnails }: Props) => {
         url: "",
         track: "",
       });
+    }
+  };
+
+  const usersRef = collection(db, "users");
+
+  // get uid of user from firebase
+  const userRef = doc(usersRef, user?.uid);
+  const userCollectionRef = collection(userRef, "user");
+  const favoritesRef = doc(userCollectionRef, "favorites");
+  const favoriteTracksRef = collection(favoritesRef, "tracks");
+
+  const [favoriteTracksSnapshot] = useCollection(favoriteTracksRef);
+
+  // console.log(favoriteTracksSnapshot)
+
+  const checkIfFavoriteExists = (videoId: string) => {
+    return favoriteTracksSnapshot?.docs.find(
+      (doc) => doc.data().videoId === videoId
+    );
+  };
+
+  const addFavorite = async () => {
+    if (!checkIfFavoriteExists(track?.videoId)) {
+      await addDoc(favoriteTracksRef, track);
+    }
+  };
+
+  const deleteFavorite = async () => {
+    const favoriteDoc = favoriteTracksSnapshot?.docs.find(
+      (doc) => doc.data().videoId === track?.videoId
+    )?.id;
+    await deleteDoc(doc(favoriteTracksRef, favoriteDoc));
+  };
+
+  const handleFavorited = () => {
+    if (checkIfFavoriteExists(track?.videoId)) {
+      deleteFavorite();
+    } else {
+      addFavorite();
     }
   };
 
@@ -90,6 +134,14 @@ const AlbumTrack = ({ track, index, thumbnails }: Props) => {
               }`}
             >
               {track.title} {track.isExplicit ? <MdExplicit /> : null}
+              <HiHeart
+                onClick={handleFavorited}
+                className={`w-50 h-4 ${
+                  checkIfFavoriteExists(track?.videoId as string)
+                    ? "text-sky-500 hover:text-sky-600 active:text-sky-700"
+                    : "text-slate-700 opacity-0 hover:text-rose-500 active:text-rose-600 group-one-hover:opacity-100 group-one-active:opacity-100"
+                } mb-0.5 transition duration-300 ease-in-out hover:cursor-pointer`}
+              />
             </span>
           </div>
           <div>
@@ -97,7 +149,7 @@ const AlbumTrack = ({ track, index, thumbnails }: Props) => {
               {/* {track?.artists.map((artist: any, index: number) => (
                 <span>{(index ? ", " : "") + artist?.name}</span>
               ))} */}
-              {track?.album?.name}
+              {/* {track?.album?.name} */}
             </span>
           </div>
         </div>
