b903304ff5a3d815eef97a764c0e440b1286ce1a
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-10-20 12:28:07 -0400

    Tilt

--- components/Album.tsx
+++ components/Album.tsx
@@ -11,6 +11,7 @@ import { auth, db } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { pipedApiUrl, tildaApiUrl } from "../utils/apiUrl";
+import Tilt from "react-parallax-tilt";
 
 interface Props {
   album: any;
@@ -75,50 +76,60 @@ const Album = ({ album }: Props) => {
 
   return (
     <div
+      onClick={() =>
+        router.push(`/playlist?list=${albumData?.audioPlaylistId}`)
+      }
       key={album.videoId}
-      className="group-one flex flex-col items-center justify-between gap-3 rounded-xl p-4 pb-8 text-sm text-slate-700 transition-all duration-300 ease-in-out hover:scale-105 hover:cursor-pointer hover:bg-slate-100 active:bg-slate-200 dark:text-white dark:hover:bg-slate-800 dark:active:bg-slate-800 dark:active:ring-1 dark:active:ring-slate-700"
+      className="group-one placeholder: flex w-48 flex-col items-center justify-between gap-3 rounded-xl bg-slate-100 p-4 pb-8 text-sm text-slate-700 transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-slate-200 active:bg-slate-200 active:ring-1 active:ring-slate-300 dark:text-white dark:hover:bg-slate-800 dark:active:bg-slate-800 dark:active:ring-1 dark:active:ring-slate-700"
     >
-      <button
-        onClick={() =>
-          router.push(`/playlist?list=${albumData?.audioPlaylistId}`)
-        }
-        className="flex flex-col gap-4"
-      >
-        <div className="group relative flex items-center justify-center overflow-hidden rounded-md transition-all">
-          <img
-            draggable={false}
-            className="h-40 w-40 rounded-xl"
-            src={album?.thumbnails[album?.thumbnails?.length - 1]?.url}
-            alt=""
-          />
-        </div>
-        <div className="flex flex-col justify-center">
+      <button className="flex flex-col gap-4">
+        <Tilt
+          glareEnable={true}
+          glareMaxOpacity={0.8}
+          glareColor="#ffffff"
+          glarePosition="bottom"
+          glareBorderRadius="12px"
+        >
+          <div className="group relative flex items-center justify-center overflow-hidden rounded-md transition-all">
+            <img
+              draggable={false}
+              className="h-40 w-40 rounded-xl"
+              src={album?.thumbnails[album?.thumbnails?.length - 1]?.url}
+              alt=""
+            />
+          </div>
+        </Tilt>
+        <div className="flex flex-col justify-start">
           <button className="flex flex-row gap-3">
             <span className="inline-flex items-center gap-1 text-base font-semibold decoration-sky-500 decoration-2 transition-colors duration-300 ease-in-out hover:underline">
               {album.title} {album.isExplicit ? <MdExplicit /> : null}
             </span>
           </button>
-          <div className="inline-flex items-center gap-1 overflow-hidden text-ellipsis">
-            <span className="font-normal">
-              {titleCase(album?.resultType)} ·{" "}
+          <div className="flex flex-col items-start gap-1.5 overflow-hidden text-ellipsis text-start">
+            <span className="text-start font-normal">
               {album?.artists.map((artist: any, index: number) => (
                 <span>{(index ? ", " : "") + artist?.name}</span>
               ))}
             </span>
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                handleFavorited();
-              }}
-            >
-              <HiHeart
-                className={`w-50 h-4 ${
-                  checkIfFavoriteExists(album?.browseId as string)
-                    ? "text-sky-500 hover:text-sky-600 active:text-sky-700"
-                    : "text-slate-700 opacity-0 hover:text-rose-500 active:text-rose-600 group-one-hover:opacity-100 group-one-active:opacity-100 dark:text-white dark:hover:text-rose-500 dark:active:text-rose-600"
-                } mb-0.5 transition duration-300 ease-in-out hover:cursor-pointer`}
-              />
-            </button>
+            <div className="inline-flex items-center gap-1">
+              <span className="rounded-full bg-slate-700 px-3 py-0.5 text-xs font-normal text-white">
+                {titleCase(album?.resultType)}
+              </span>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleFavorited();
+                }}
+              >
+                <HiHeart
+                  className={`w-50 h-4 ${
+                    checkIfFavoriteExists(album?.browseId as string)
+                      ? "text-sky-500 hover:text-sky-600 active:text-sky-700"
+                      : "text-slate-700 opacity-0 hover:text-rose-500 active:text-rose-600 group-one-hover:opacity-100 group-one-active:opacity-100 dark:text-white dark:hover:text-rose-500 dark:active:text-rose-600"
+                  } mb-0.5 transition duration-300 ease-in-out hover:cursor-pointer`}
+                />
+              </button>
+            </div>
           </div>
         </div>
       </button>
