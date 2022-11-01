4869f3844e3e3e37ca9977a97235615c4da2235d
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-11-01 12:37:22 -0400

    apiUrl

--- utils/apiUrl.ts
+++ utils/apiUrl.ts
@@ -1,7 +1,7 @@
 const tildaApiUrl = "https://tilda-api.deta.dev";
 // const tildaApiUrl = "https://tilda-api.ayaanzaveri.repl.co";
-// const pipedApiUrl = "https://pipedapi.kavin.rocks";
-const pipedApiUrl = "https://pa.il.ax";
+const pipedApiUrl = "https://pipedapi.kavin.rocks";
+// const pipedApiUrl = "https://pa.il.ax";
 // const pipedApiUrl = "https://watchapi.whatever.social";
 
 export { tildaApiUrl, pipedApiUrl };
