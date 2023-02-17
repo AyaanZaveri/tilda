fc843fbd6f83f282b8418f6b1a599a46a023b4f3
Ayaan <63752541+AyaanZaveri@users.noreply.github.com>
2023-02-17 18:45:38 +0000

    Jellyfin SDK

--- pages/jellyfin.tsx
+++ pages/jellyfin.tsx
@@ -13,8 +13,43 @@ import AudioPlayer from "react-h5-audio-player";
 import { MdExplicit } from "react-icons/md";
 import { useRecoilState } from "recoil";
 import { currentTrackState } from "../atoms/songAtom";
+import { useEffect } from "react";
+// Jellyfin SDK
+import { Jellyfin } from '@jellyfin/sdk';
+import { getItemsApi } from '@jellyfin/sdk/lib/utils/api/items-api.js';
+import { ItemFields } from '@jellyfin/sdk/lib/generated-client/models/item-fields.js';
 
 const Home: NextPage = () => {
+
+  (async () => {
+    const jellyfin = new Jellyfin({
+      clientInfo: {
+        name: "My Client Application",
+        version: "1.0.0",
+      },
+      deviceInfo: {
+        name: "Device Name",
+        id: "unique-device-id",
+      },
+    });
+    const api = jellyfin.createApi('https://revised-tribunal-telephony-arise.trycloudflare.com');
+    const auth = await api.authenticateUserByName('abc', 'abc');
+    const user: any = auth.data.User;
+  
+    const items: any = await getItemsApi(api).getItemsByUserId({
+      userId: user?.Id,
+      fields: [ ItemFields.Path ],
+      recursive: true
+    });
+  
+    items.data.Items.forEach((item: any) => {
+      console.log(item)
+      // console.log(`${item.Id}\t${item.Name}\t${item.Path}`);
+    });
+
+    console.log(items)
+  })();
+
   return <div></div>;
 };
 
