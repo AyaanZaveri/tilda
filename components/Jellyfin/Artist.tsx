d88a48cbc0757bd2bd7f1435832c6469a7586c94
Ayaan <63752541+AyaanZaveri@users.noreply.github.com>
2023-02-17 20:51:05 +0000

    Albums & Artists

---
+++ components/Jellyfin/Artist.tsx
@@ -0,0 +1,41 @@
+import Tilt from "react-parallax-tilt";
+
+interface Props {
+  artist: any;
+}
+
+const Artist = ({ artist }: Props) => {
+  return (
+    <div
+      className="group-one flex w-48 flex-col items-center justify-between gap-3 rounded-xl bg-white p-4 pb-4 text-sm text-slate-700  transition-all duration-300 ease-in-out hover:cursor-pointer hover:bg-slate-100 active:ring-slate-200 dark:bg-slate-900 dark:text-white dark:hover:bg-slate-800 dark:active:ring-1 dark:active:ring-slate-700"
+    >
+      <button className="flex flex-col gap-4 items-center">
+        <Tilt
+          glareEnable={true}
+          glareMaxOpacity={0.8}
+          glareColor="#ffffff"
+          glarePosition="bottom"
+          glareBorderRadius="12px"
+        >
+          <div className="group relative flex items-center justify-center overflow-hidden rounded-md transition-all">
+            <img
+              draggable={false}
+              className="h-40 w-40 rounded-xl"
+                src={artist?.ImageTags?.Primary ? `https://revised-tribunal-telephony-arise.trycloudflare.com/Items/${artist?.Id}/Images/Primary?maxHeight=400&tag=${artist?.ImageTags?.Primary}&quality=90` : "/images/artist.png"}
+              alt=""
+            />
+          </div>
+        </Tilt>
+        <div className="flex flex-col justify-center">
+          <button className="flex flex-row gap-3">
+            <span className="inline-flex items-center gap-1 text-center text-base font-semibold decoration-emerald-500 decoration-2 transition-colors duration-300 ease-in-out hover:underline">
+                {artist.Name}
+            </span>
+          </button>
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default Artist;
--- pages/jellyfin.tsx
+++ pages/jellyfin.tsx
@@ -13,15 +13,22 @@ import AudioPlayer from "react-h5-audio-player";
 import { MdExplicit } from "react-icons/md";
 import { useRecoilState } from "recoil";
 import { currentTrackState } from "../atoms/songAtom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 // Jellyfin SDK
-import { Jellyfin } from '@jellyfin/sdk';
-import { getItemsApi } from '@jellyfin/sdk/lib/utils/api/items-api.js';
-import { ItemFields } from '@jellyfin/sdk/lib/generated-client/models/item-fields.js';
+import { Jellyfin } from "@jellyfin/sdk";
+import { getItemsApi } from "@jellyfin/sdk/lib/utils/api/items-api";
+import { getArtistsApi } from "@jellyfin/sdk/lib/utils/api/artists-api";
+import { ItemFields } from "@jellyfin/sdk/lib/generated-client/models/item-fields.js";
+import Artist from "../components/Jellyfin/Artist";
 
 const Home: NextPage = () => {
+  const [api, setApi] = useState<any>();
+  const [user, setUser] = useState<any>(null);
 
-  (async () => {
+  const [artists, setArtists] = useState<any>(null);
+  const [albums, setAlbums] = useState<any>(null);
+
+  const handleJellyfin = async () => {
     const jellyfin = new Jellyfin({
       clientInfo: {
         name: "My Client Application",
@@ -32,25 +39,63 @@ const Home: NextPage = () => {
         id: "unique-device-id",
       },
     });
-    const api = jellyfin.createApi('https://revised-tribunal-telephony-arise.trycloudflare.com');
-    const auth = await api.authenticateUserByName('abc', 'abc');
-    const user: any = auth.data.User;
-  
-    const items: any = await getItemsApi(api).getItemsByUserId({
-      userId: user?.Id,
-      fields: [ ItemFields.Path ],
-      recursive: true
-    });
-  
-    items.data.Items.forEach((item: any) => {
-      console.log(item)
-      // console.log(`${item.Id}\t${item.Name}\t${item.Path}`);
-    });
+    const capi = jellyfin.createApi(
+      "https://revised-tribunal-telephony-arise.trycloudflare.com"
+    );
+    const auth = await capi.authenticateUserByName("abc", "abc");
+    setUser(auth.data.User);
 
-    console.log(items)
-  })();
+    setApi(capi);
+  };
 
-  return <div></div>;
+  useEffect(() => {
+    handleJellyfin();
+  }, []);
+
+  const getJellyfinData = async () => {
+    if (api) {
+      const artistsData: any = await getArtistsApi(api).getArtists({
+        userId: user?.Id,
+      });
+
+      setArtists(artistsData);
+
+      const items: any = await getItemsApi(api).getItemsByUserId({
+        userId: user.Id,
+        sortBy: "SortName" as any,
+        sortOrder: "Ascending" as any,
+        recursive: true,
+        excludeLocationTypes: "Virtual" as any,
+        includeItemTypes: "MusicAlbum" as any,
+      });
+
+      setAlbums(items.data.Items);
+    } else {
+      console.log("no api, haha you suck lol");
+    }
+  };
+
+  useEffect(() => {
+    getJellyfinData();
+  }, [user]);
+
+  console.log("albums", albums);
+
+  return (
+    <div className="ml-3 pl-64 pr-12">
+      <div className="pt-[4.5rem] pb-8">
+        <div className="pt-6">
+          <h1 className="text-3xl font-semibold">Artists</h1>
+          <div className="flex flex-row flex-wrap gap-x-8 gap-y-2 pt-4">
+            {/* map out the artists into components */}
+            {artists?.data?.Items?.map((artist: any) => (
+              <Artist artist={artist} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
